30ee6bc9ba060e32523f0c3d0c4feb5602508048
dssantos <danilosantos1986@gmail.com>
2021-12-22 23:13:26 -0300

    Added login API

--- news_api/views.py
+++ news_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from news_api import serializers
 from news_api import models
@@ -20,3 +22,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
             return models.UserProfile.objects.all()
         queryset = models.UserProfile.objects.filter(id=self.request.user.id)
         return queryset
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
