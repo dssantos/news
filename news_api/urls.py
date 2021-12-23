0c7d8e49bade936732eee667c50013f6c3e23899
dssantos <danilosantos1986@gmail.com>
2021-12-22 22:37:08 -0300

    Added viewset to profiles api

---
+++ news_api/urls.py
@@ -0,0 +1,13 @@
+from django.db import router
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from news_api import views
+
+
+router = DefaultRouter()
+router.register('profile', views.UserProfileViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- news_api/views.py
+++ news_api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from news_api import serializers
+from news_api import models
+from news_api import permissions
+
+        
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permissions_classes = (permissions.UpdateOwnProfile,  IsAuthenticated)
+
+    def get_queryset(self):
+        """restricts the returned profile to a given user, except if adminuser"""
+        if self.request.user.is_staff:
+            return models.UserProfile.objects.all()
+        queryset = models.UserProfile.objects.filter(id=self.request.user.id)
+        return queryset
